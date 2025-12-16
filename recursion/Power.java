3cbcb5ee4ffee0b4b7857ae88ab8d25c0627d97e
Ekshith <ekshithkumarmamidisetti@gmail.com>
2025-12-16 11:18:44 +0530

    power function using recursion

--- recursion/Power.java
+++ recursion/Power.java
@@ -1,4 +1,11 @@
 package recursion;
 
 public class Power {
+    public static int pow(int m,int n){
+        if(n==0) return 1;
+        return pow(m,n-1)*m;
+    }
+    public static void main(String []ar){
+        System.out.println(pow(2,5));
+    }
 }
