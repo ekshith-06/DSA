31338d387f9658964d5cfbe82c04fc4e75a9c75f
Ekshith <ekshithkumarmamidisetti@gmail.com>
2025-12-16 10:19:58 +0530

    Sum Of Natural Nos Recursion

---
+++ recursion/SumNatural.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class SumNatural {
+    public static int sum(int n ){
+        if(n==0) return 0;
+        return sum(n-1)+n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+}
