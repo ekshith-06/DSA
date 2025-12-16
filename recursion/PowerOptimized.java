0735e6a05a486822fd62a33946b20618a3d77a87
Ekshith <ekshithkumarmamidisetti@gmail.com>
2025-12-16 11:17:59 +0530

    optimized power fuction

--- recursion/PowerOptimized.java
+++ recursion/PowerOptimized.java
@@ -1,4 +1,12 @@
 package recursion;
 
 public class PowerOptimized {
+    public static int pow(int m, int n){
+        if(n==0) return  1;
+        else if(n%2==0) return pow(m*m,n/2);
+        else return pow(m*m,n/2)*m;
+    }
+    public static void main(String []ar){
+        System.out.println(pow(2,5));
+    }
 }
